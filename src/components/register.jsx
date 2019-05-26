461d684394a12aed3312c19d7db1992bfaf3f2d0
RachitMalik12 <rachitmalik1947@gmail.com>
2019-05-26 15:23:27 +0530

    Minor fixes

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -12,8 +12,8 @@ class Register extends Form {
 
 
      schema = {
-        username: Joi.string().required().label("Username"), 
-        password: Joi.string().required().label("Password"),
+        username: Joi.string().required().email().label("Username"), 
+        password: Joi.string().required().min(5).label("Password"),
         name: Joi.string().required().label("Name")
     }
     doSubmit = () =>{
