6696cfec2051608abf7660dab90209b4a581408a
RachitMalik12 <rachitmalik1947@gmail.com>
2019-05-26 15:21:49 +0530

    Added login and registration forms with validation using Joi

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Rentals from './components/Rentals';
 import NotFound from './components/NotFound';
 import NavBar from './components/navbar';
 import MovieForm from './components/movieForm';
+import LoginForm from './components/loginForm';
+import Register from './components/register';
 
 function App() {
   return (
@@ -16,10 +18,12 @@ function App() {
     <main className = "container">
      
       <Switch>
+        <Route path="/login" component = {LoginForm} />
         <Route path="/customers" component = {Customers} />
         <Route path = "/rentals" component = {Rentals} />
         <Route path = "/movies/:id" component = {MovieForm}/>
         <Route path = "/movies" component = {Movies} />
+        <Route path = "/register" component = {Register} />
         <Route path = "/not-found" component = {NotFound} />
         <Route path = "/"  exact component = {Movies} />
        
---
+++ src/components/register.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Form from '../common/form';
+import Joi from 'joi-browser';
+
+
+class Register extends Form {
+    state = { 
+       data: {username:'', password: '', name:''},
+       errors: {}
+     }
+
+
+
+     schema = {
+        username: Joi.string().required().label("Username"), 
+        password: Joi.string().required().label("Password"),
+        name: Joi.string().required().label("Name")
+    }
+    doSubmit = () =>{
+        console.log("Registered");
+    }
+    render() { 
+        return ( 
+            <div>
+                <h1> Register </h1>
+                <form onSubmit={this.handleSubmit}>
+                    {this.renderInput('username','Username','email')}
+                    {this.renderInput('password','Password','password')}
+                    {this.renderInput('name','Name')}
+                    {this.renderButton('Register')}
+                </form>
+            </div>
+         );
+    }
+}
+ 
+export default Register;
